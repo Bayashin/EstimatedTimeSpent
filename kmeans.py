07420e66879fc7494a33d5d443639314cb15c145
Bayashin <k21099kk@aitech.ac.jp>
2023-12-24 18:33:54 +0900

    fix:グラフの描画を別ファイルにした

--- kmeans.py
+++ kmeans.py
@@ -1,9 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import japanize_matplotlib
 from datetime import datetime
-import matplotlib.dates as mdates
 
 def convert_seconds_to_hms(seconds):
     hours, remainder = divmod(seconds, 3600)
@@ -42,44 +39,6 @@ def k_means_clustering(data, k, centroids):
     
     return clusters
 
-def make_graph(clusters, title):
-    # データを日時オブジェクトに変換
-    data = [datetime.strptime(time, "%H:%M:%S.%f") if '.' in time else datetime.strptime(time, "%H:%M:%S") for time in df_entry]
-
-    # クラスタのデータを日時オブジェクトに変換（ミリ秒を含む）
-    cluster_data = {cluster['centroid']: [datetime.strptime(time, "%H:%M:%S.%f") if '.' in time else datetime.strptime(time, "%H:%M:%S") for time in cluster['points']] for cluster in clusters}
-
-    # グラフの作成
-    fig, ax = plt.subplots(figsize=(12, 6))
-    for cluster, times in cluster_data.items():
-        ax.scatter(times, [1] * len(times), label=f"Cluster {list(cluster_data.keys()).index(cluster) + 1}")
-
-# クラスタのセントロイドをプロット
-    for cluster in clusters:
-        centroid_time = datetime.strptime(cluster['centroid'], "%H:%M:%S")
-        ax.scatter(centroid_time, 1, c='black', marker='x', s=100, label=f"Centroid {clusters.index(cluster) + 1} ")
-
-# グラフの設定
-    ax.yaxis.set_visible(False)  # y軸を非表示にする
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M'))  # x軸のフォーマットを設定
-    plt.xlabel('時間')
-    plt.title(title)
-    plt.legend()
-
-    # グラフを表示
-    plt.show()
-
-# 入力データのパース
-# time_data = [
-#     "08:11:41.859", "08:22:22.226", "08:46:37.590", "08:46:01.208", "08:36:44.679",
-#     "08:40:49.586", "08:46:28.548", "09:59:28.740", "08:35:32.321", "10:22:07.852",
-#     "08:33:45.183", "08:07:22.920", "08:34:16.666", "08:40:08.239", "09:10:07.362",
-#     "08:29:42.010", "08:24:35.053", "08:34:03.306", "08:38:46.798", "08:45:19.724",
-#     "08:26:56.303", "08:18:04.055", "08:19:08.538", "08:25:08.055", "15:32:34.266",
-#     "08:33:10.705", "08:25:39.100", "08:29:43.298", "08:29:08.290", "08:33:43.808",
-#     "08:32:04.731"
-# ]
-
 # CSVファイルからデータを読み込む
 file_path = 'processed_data/hayashi_09-10.csv'  # ファイルパスを適切に設定
 df = pd.read_csv(file_path, delimiter=',')  # タブ区切りの場合
@@ -101,13 +60,9 @@ result_first = k_means_clustering(data_seconds_first, k, centroids)
 result_end = k_means_clustering(data_seconds_end, k, centroids)
 
 # 結果の出力
-# print(result_first)
-# for i, cluster in enumerate(result_first):
-    # centroid_time = cluster["centroid"]
-    # cluster_points = cluster["points"]
+print(result_first)
+for i, cluster in enumerate(result_first):
+    centroid_time = cluster["centroid"]
+    cluster_points = cluster["points"]
     
-    # print(f"Cluster {i + 1}: Centroid = {centroid_time}, Points = {cluster_points}")
-
-# グラフの作成
-make_graph(result_first, 'hayashiの入室時間')
-make_graph(result_end, 'hayashiの退室時間')
+    print(f"Cluster {i + 1}: Centroid = {centroid_time}, Points = {cluster_points}")
