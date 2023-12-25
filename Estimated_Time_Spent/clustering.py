b64fba5d28436985d075476d69edd047b7713054
Bayashin <k21099kk@aitech.ac.jp>
2023-12-25 20:07:14 +0900

    add:xmeansの実装

--- Estimated_Time_Spent/clustering.py
+++ Estimated_Time_Spent/clustering.py
@@ -7,7 +7,9 @@ def convert_seconds_to_hms(seconds):
     minutes, seconds = divmod(remainder, 60)
     return f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}"
 
-def k_means_clustering(data, k, centroids):
+def k_means_clustering(data, k):
+    # ランダムな点をk個設置
+    centroids = np.random.choice(data, size=k, replace=False)
     
     while True:
         # ステップ2: 最も近い点が同じデータでグループ化
@@ -36,5 +38,40 @@ def k_means_clustering(data, k, centroids):
             "centroid": centroid_time,
             "points": cluster_points
         })
+    # print(clusters)
+    return clusters
+
+def xmeans(data):
+    # 初期クラスタ数
+    k = 1
     
+    while True:
+        # K-means法でクラスタリング
+        clusters = k_means_clustering(data, k)
+        # for i, cluster in enumerate(clusters):
+        #     centroid_time = cluster["centroid"]
+        #     cluster_points = cluster["points"]
+        #     print(f"Cluster {i + 1}: Centroid = {centroid_time}, Points = {cluster_points}")
+
+        # クラスタごとにデータポイントを秒に変換
+        data_seconds = [[sum(x * int(t) for x, t in zip([3600, 60, 1], point.split(":"))) for point in cluster['points']] for cluster in clusters]
+        # print(data_seconds)
+
+        # クラスタごとにデータ分散を計算
+        cluster_variances = [np.var(cluster_data) for cluster_data in data_seconds]
+
+        # クラスタ内のデータ分散の平均を計算
+        avg_cluster_variance = np.mean(cluster_variances)
+        
+        # print(cluster_variances)
+        # print(f"クラスタ数: {k}, クラスタ内のデータ分散の平均: {avg_cluster_variance}")
+        print("")
+        
+        # クラスタ内のデータ分散が閾値以下なら終了
+        if avg_cluster_variance < 35000000:
+            break
+        
+        # クラスタ数を増やして再実行
+        k += 1
+
     return clusters
--- Estimated_Time_Spent/main.py
+++ Estimated_Time_Spent/main.py
@@ -3,34 +3,30 @@ import Estimated_Time_Spent. graph as graph
 import pandas as pd
 import numpy as np
 
-# CSVファイルからデータを読み込む
-file_path = 'processed_data/hayashi_09-10.csv'  # ファイルパスを適切に設定
-df = pd.read_csv(file_path, delimiter=',')  # タブ区切りの場合
-# 時間の文字列をdatetime型に変換
-df_entry = df['first_entry'].to_list()
-df_exit = df['last_exit'].to_list()
+def main():
+    # CSVファイルからデータを読み込む
+    file_path = 'processed_data/hayashi_09-10.csv'  # ファイルパスを適切に設定
+    df = pd.read_csv(file_path, delimiter=',')  # タブ区切りの場合
+    # 時間の文字列をdatetime型に変換
+    df_entry = df['entry'].to_list()
+    df_exit = df['exit'].to_list()
 
-# 時間データを秒単位に変換
-data_seconds_first = np.array([sum(x * float(t) for x, t in zip([3600, 60, 1], point.split(":"))) for point in df_entry])
-data_seconds_end   = np.array([sum(x * float(t) for x, t in zip([3600, 60, 1], point.split(":"))) for point in df_exit])
+    # 時間データを秒単位に変換
+    data_seconds_first = np.array([sum(x * float(t) for x, t in zip([3600, 60, 1], point.split(":"))) for point in df_entry])
+    # data_seconds_end   = np.array([sum(x * float(t) for x, t in zip([3600, 60, 1], point.split(":"))) for point in df_exit])
 
-# クラスタリングの実行
-k = 3  # クラスタの数
-# 初期値を設定
-initial_centroids = ["6:00", "12:00", "18:00"]
-# 初期値を秒単位に変換
-centroids = np.array([sum(x * int(t) for x, t in zip([3600, 60], point.split(":"))) for point in initial_centroids])
-result_first = clustering.k_means_clustering(data_seconds_first, k, centroids)
-result_end = clustering.k_means_clustering(data_seconds_end, k, centroids)
+    # クラスタリングの実行
+    result_first = clustering.xmeans(data_seconds_first)
+    # result_end = clustering.xmeans(data_seconds_end)
 
-# 結果の出力
-print(result_first)
-for i, cluster in enumerate(result_first):
-    centroid_time = cluster["centroid"]
-    cluster_points = cluster["points"]
-    
-    print(f"Cluster {i + 1}: Centroid = {centroid_time}, Points = {cluster_points}")
+    # 結果の出力
+    # print(result_first)
+    # for i, cluster in enumerate(result_first):
+    #     centroid_time = cluster["centroid"]
+    #     cluster_points = cluster["points"]
+        
+    #     print(f"Cluster {i + 1}: Centroid = {centroid_time}, Points = {cluster_points}")
 
-# グラフの作成
-graph.make_graph(result_first, "hayashiの入室時刻")
-graph.make_graph(result_end, "hayashiの退室時刻")
+    # グラフの作成
+    graph.make_graph(result_first, "hayashiの入室時刻")
+    # graph.make_graph(result_end, "hayashiの退室時刻")
