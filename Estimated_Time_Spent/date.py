e06bb6ae7249125d39848f6999563688ebeed774
Bayashin <k21099kk@aitech.ac.jp>
2024-01-10 22:27:25 +0900

    fix:関数の整理

--- Estimated_Time_Spent/date.py
+++ Estimated_Time_Spent/date.py
@@ -2,13 +2,11 @@ import pandas as pd
 from datetime import datetime
 
 def identification_day(df = pd.DataFrame()):
-    # 日付から曜日を取得してDataFrameに追加
-    # CSVファイルからデータを読み込む
-    # file_path = 'processed_data/hayashi_09-10.csv'  # ファイルパスを適切に設定
-    # df = pd.read_csv(file_path, delimiter=',')  # タブ区切りの場合
+    
     # 時間の文字列をdatetime型に変換
     df_date= df['date'].to_list()
 
+    # 日付から曜日を取得してDataFrameに追加
     day = []
     for i in range(len(df_date)):
         date_object = datetime.strptime(df_date[i], '%Y-%m-%d')
@@ -18,3 +16,7 @@ def identification_day(df = pd.DataFrame()):
     # print(df)
     return df
 
+def convert_seconds_to_hms(seconds):
+    hours, remainder = divmod(seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return f"{int(hours):02d}:{int(minutes):02d}:{int(seconds):02d}"
