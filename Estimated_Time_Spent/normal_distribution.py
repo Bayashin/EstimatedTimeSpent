db7a7f6cff690221f413c716c91a24f28cd845a9
Bayashin <k21099kk@aitech.ac.jp>
2024-01-15 19:54:34 +0900

    update:累積分布関数に重みをつけて計算するようにした

--- Estimated_Time_Spent/normal_distribution.py
+++ Estimated_Time_Spent/normal_distribution.py
@@ -9,7 +9,7 @@ def convert_to_seconds(time_str):
     return hours * 3600 + minutes * 60 + seconds
 
 def normal_distribution(data, label, color):
-    print(data)
+    # print(data)
     # 時刻データを秒に変換
     time_seconds = np.array([convert_to_seconds(t) for t in data])
 
@@ -21,7 +21,7 @@ def normal_distribution(data, label, color):
     plt.plot(x_vals, pdf_vals, color=color, label=label)
 
 def density_estimation_graph(data):
-    print(data)
+    # print(data)
     # データセットごとに確率密度関数をプロット
     if len(data) == 1:
         normal_distribution(data[0], 'Dataset 1', 'red')
@@ -38,7 +38,7 @@ def density_estimation_graph(data):
     plt.show()
 
 # timeが発生する確率を計算
-def probability_from_normal_distribution(data, time):
+def probability_from_normal_distribution(data, time, day_count):
     l=0
     probabilities = []
     for i, datum in enumerate(data):
@@ -55,10 +55,10 @@ def probability_from_normal_distribution(data, time):
             continue
         loc = np.mean(data_seconds)
         scale = np.std(data_seconds)
-        probabilities.append(norm.cdf(time_seconds, loc, scale))
+        probabilities.append(norm.cdf(time_seconds, loc, scale)* len(data_seconds)/day_count)
         print(f"{l+1}番目の確率: {probabilities[l]*100:.2f}%")
         l += 1
     # timeより前に研究室に来ている割合を計算
-    probability = np.mean(probabilities)
+    probability = np.sum(probabilities)
     print(f"{time}までに研究室に来ている確率は{probability*100:.2f}%です。")
     print(f"{convert_to_seconds(time)}までに研究室に来ている確率は{probability*100:.2f}%です。")
