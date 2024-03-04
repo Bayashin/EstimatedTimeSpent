1ca3e7ded235dbade0d3bea796b9bbdda3212f61
Bayashin <k21099kk@aitech.ac.jp>
2024-03-05 02:23:12 +0900

    add:rootパスの追加

--- api/main.py
+++ api/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-import uvicorn
 
+from controller.root import router as root_router
 from controller.probability import router as probability_router
 
 app = FastAPI()
 
+app.include_router(root_router)
 app.include_router(probability_router)
